ec8a972151a183dbc95455edd6a2857d42bd3745
David Vishwakarma <david.vishwakarma@elitemantra.com>
2023-02-18 17:36:52 +0530

    button space

--- app/view/CreateAd/CreateAds.js
+++ app/view/CreateAd/CreateAds.js
@@ -1,94 +1,96 @@
-import { View, Text, KeyboardAvoidingView,Alert } from 'react-native'
+import {View, Text, KeyboardAvoidingView, Alert} from 'react-native';
 import React, {useState} from 'react';
-import { TextInput,Button } from 'react-native-paper';
+import {TextInput, Button} from 'react-native-paper';
 import auth from '@react-native-firebase/auth';
 import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 import storage from '@react-native-firebase/storage';
 import firestore from '@react-native-firebase/firestore';
-import { styles } from './CreateAds.style';
-
-
+import {styles} from './CreateAds.style';
 
 const CreateAds = () => {
-  const [name,setName] = useState('')
-  const [desc,setDesc] = useState('')
-  const [year, setYear] = useState('')
-  const [price, setPrice] = useState('')
-  const [phone, setPhone] = useState('')
-  const [image, setImage] = useState('')
+  const [name, setName] = useState('');
+  const [desc, setDesc] = useState('');
+  const [year, setYear] = useState('');
+  const [price, setPrice] = useState('');
+  const [phone, setPhone] = useState('');
+  const [image, setImage] = useState('');
 
   const sendNotification = () => {
-    firestore().collection('usertoken').get().then(querySnap=>{
-     const userDevicetoken = querySnap.docs.map(docSnap=>{
-        return docSnap.data().token
-      })
-      console.log(userDevicetoken)
-      fetch('https://66d3-103-203-230-34.ngrok.io/send-noti'), ({
-        method:'post',
-        headers: {
-          'Content-Type': 'application/json'
-         
-        },
-        body:JSON.stringify({
-          tokens:userDevicetoken
-        })
-      })
-    })
-  }
+    firestore()
+      .collection('usertoken')
+      .get()
+      .then(querySnap => {
+        const userDevicetoken = querySnap.docs.map(docSnap => {
+          return docSnap.data().token;
+        });
+        console.log(userDevicetoken);
+        fetch('https://66d3-103-203-230-34.ngrok.io/send-noti'),
+          {
+            method: 'post',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              tokens: userDevicetoken,
+            }),
+          };
+      });
+  };
 
   const postData = async () => {
-    if(!name||!desc||!year||!price||!phone) {
+    if (!name || !desc || !year || !price || !phone) {
       Alert.alert('Please all the fields');
-      return
-  } 
+      return;
+    }
     try {
-    const result =  await firestore().collection('ads')
-      .add({
+      const result = await firestore().collection('ads').add({
         name,
         desc,
         year,
         price,
         phone,
         image,
-        uid:auth().currentUser.uid
-      })
-      Alert.alert('Posted you Ads.')
+        uid: auth().currentUser.uid,
+      });
+      Alert.alert('Posted you Ads.');
     } catch (error) {
-      Alert.alert('Something Wrong try again.')
+      Alert.alert('Something Wrong try again.');
     }
-  }
+  };
 
   const openCamera = () => {
-    launchImageLibrary({quality:0.5},(fileObj) =>{
-      console.log(fileObj.assets[0].uri)
-     const uploadTask = storage().ref().child(`/items/${Date.now()}`).putFile(fileObj.assets[0].uri.split('file://')[1]);
-     uploadTask.on('state_changed', 
-        (snapshot) => {
-          var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    launchImageLibrary({quality: 0.5}, fileObj => {
+      console.log(fileObj.assets[0].uri);
+      const uploadTask = storage()
+        .ref()
+        .child(`/items/${Date.now()}`)
+        .putFile(fileObj.assets[0].uri.split('file://')[1]);
+      uploadTask.on(
+        'state_changed',
+        snapshot => {
+          var progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
           //console.log('Upload is ' + progress + '% done');
-          if(progress==100){
-            alert("uploaded")
-
+          if (progress == 100) {
+            alert('uploaded');
           }
-        }, 
-        (error) => {
-          alert(error)
-        }, 
+        },
+        error => {
+          alert(error);
+        },
         () => {
           // Handle successful uploads on complete
           // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-          uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+          uploadTask.snapshot.ref.getDownloadURL().then(downloadURL => {
             //console.log('File available at', downloadURL);
-            setImage(downloadURL)
+            setImage(downloadURL);
           });
-        }
+        },
       );
-          })
-        }
-
+    });
+  };
 
   return (
-   
     <View style={styles.container}>
       <Text style={styles.text}>CreateAdsScreen</Text>
       <TextInput
@@ -96,52 +98,56 @@ const CreateAds = () => {
         value={name}
         onChangeText={text => setName(text)}
         mode="outlined"
-        />
-         <TextInput
+      />
+      <TextInput
         label="Descriptions"
         value={desc}
         numberOfLines={3}
         onChangeText={text => setDesc(text)}
         mode="outlined"
-        />
-         <TextInput
+      />
+      <TextInput
         label="Year of purchase"
         value={year}
         keyboardType="numeric"
         onChangeText={text => setYear(text)}
         mode="outlined"
-        />
-         <TextInput
+      />
+      <TextInput
         label="Price in INR"
         value={price}
         keyboardType="numeric"
         onChangeText={text => setPrice(text)}
         mode="outlined"
-        />
-         <TextInput
+      />
+      <TextInput
         label="Your Contact Number"
         value={phone}
         keyboardType="numeric"
         onChangeText={text => setPhone(text)}
         mode="outlined"
-        />
-       <Button  icon="camera" mode="contained" onPress={() => sendNotification()}>
-      send notification
+      />
+      <Button
+        icon="camera"
+        mode="contained"
+        onPress={() => sendNotification()}
+        style={{marginVertical: 10}}>
+        send notification
+      </Button>
+      <Button icon="camera" mode="contained" onPress={() => openCamera()}>
+        Upload Image
+      </Button>
+      <View style={styles.box2}>
+        <Button
+          disabled={image ? false : true}
+          style={{marginHorizontal: 20}}
+          mode="contained"
+          onPress={() => postData()}>
+          Post
         </Button>
-        <Button  icon="camera" mode="contained" onPress={() => openCamera()}>
-               Upload Image
-        </Button>
-        <View style={styles.box2}>
-        <Button disabled={image?false:true} style={{marginHorizontal:20}} mode="contained" onPress={() => postData()}>
-                Post
-             </Button>
-        </View>
-             
+      </View>
     </View>
-    
-  )
-}
+  );
+};
 
-
-
-export default CreateAds
+export default CreateAds;
--- app/view/CreateAd/CreateAds.style.js
+++ app/view/CreateAd/CreateAds.style.js
@@ -1,17 +1,18 @@
-import { StyleSheet } from 'react-native'
-import React from 'react'
+import {StyleSheet} from 'react-native';
+import React from 'react';
 
 export const styles = StyleSheet.create({
-    container :{
-      marginHorizontal:30,
-      justifyContent:'space-evenly',
-     
-    },
-    text : {
-      fontSize:30,
-      textAlign:'center'
-    },
-    box2:{
-      marginVertical:20
-    }
-  })
+  container: {
+    marginHorizontal: 30,
+    justifyContent: 'space-evenly',
+  },
+  text: {
+    color: '#000',
+    fontSize: 30,
+    textAlign: 'center',
+    paddingVertical:10
+  },
+  box2: {
+    marginVertical: 20,
+  },
+});
